1dfa1403f1a2454d6056fd7648cb6caeef6bb8de
Berke Algül <47500887+berkealgul@users.noreply.github.com>
2020-06-01 11:51:25 +0300

    Add files via upload

--- DeepQ_Learning/main.py
+++ DeepQ_Learning/main.py
@@ -2,64 +2,66 @@ import numpy as np
 from dql import Agent
 from utils import plot_learning_curve, make_env
 
-def main():
-    env_name = 'PongNoFrameskip-v4'
-    env = make_env(env_name)
-    best_score = -np.inf
 
-    train = True
-    load_checkpoint = True
+# Train Mode parameters
+train = True
+load_checkpoint = True
+render = True
+n_games = 500
+######################
 
-    n_games = 500
-    agent = Agent(lr=0.001, gamma=0.99, epsilon=1.0, env_name=env_name,
-            n_actions=env.action_space.n, in_dims=env.observation_space.shape,
-            batch_size=32, mem_size=2500)
+env_name = 'PongNoFrameskip-v4'
+env = make_env(env_name)
+best_score = -np.inf
 
-    plot = 'plots/atari.png'
+agent = Agent(lr=0.001, gamma=0.99, epsilon=1.0, env_name=env_name,
+        n_actions=env.action_space.n, in_dims=env.observation_space.shape,
+        batch_size=32, mem_size=2500)
 
-    if load_checkpoint:
-        agent.load_model()
+plot = 'plots/atari.png'
 
-    scores, eps_history, steps_history = [], [], []
-    n_steps = 0
+if load_checkpoint:
+    agent.load_model()
 
-    for i in range(n_games):
-        done = False
-        score = 0
-        obs = env.reset()
+scores, eps_history, steps_history = [], [], []
+n_steps = 0
 
-        while not done:
-            action = agent.choice_action(obs)
-            obs_, reward, done, info = env.step(action)
-            score += reward
+for i in range(n_games):
+    done = False
+    score = 0
+    obs = env.reset()
 
-            if train:
-                agent.store_translition(obs, obs_, action, reward, int(done))
-                agent.train()
+    while not done:
+        action = agent.choice_action(obs)
+        obs_, reward, done, info = env.step(action)
+        score += reward
 
-            #env.render()
+        if train:
+            agent.store_translition(obs, obs_, action, reward, int(done))
+            agent.train()
 
-            n_steps += 1
-            obs = obs_
+        env.render()
 
-        avg_score = np.mean(scores[-100:])
+        n_steps += 1
+        obs = obs_
 
-        if avg_score > best_score:
-            best_score = avg_score
-            if train:
-                agent.save_model()
+    avg_score = np.mean(scores[-100:])
 
-        plot_learning_curve(steps_history, scores, eps_history, plot)
-
-        print("Step: ", n_steps, "Game: ", i, "avg score: ", avg_score,
-        "epsilon: ", agent.epsilon)
-
-        scores.append(score)
-        eps_history.append(agent.epsilon)
-        steps_history.append(n_steps)
+    if avg_score > best_score:
+        best_score = avg_score
+        if train:
+            agent.save_model()
 
 
 
+    print("Step: ", n_steps, "Game: ", i, "avg score: ", avg_score,
+    "epsilon: ", agent.epsilon)
 
-if __name__ == "__main__":
-    main()
+    scores.append(score)
+    eps_history.append(agent.epsilon)
+    steps_history.append(n_steps)
+
+    plot_learning_curve(steps_history, scores, eps_history, plot)
+
+
+print("Similation Done")
